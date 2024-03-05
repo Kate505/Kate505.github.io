1ac21ef4334bf555e40d55a03baf4f2c121519fd
Kate505 <ksdelgado31@gmail.com>
2024-03-05 09:24:58 -0600

    src and public directories added

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const LINKS = {
+  github: "https://github.com/Kate505",
+  linkedin: "https://www.linkedin.com/in/kate-505/",
+  medium: "https://ladvace.medium.com/",
+  discord: "https://discordapp.com/users/163300027618295808",
+  dribble: "https://dribbble.com/Ladvace_Jace",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
