8ea0957b53a97138dba8fe9c2513f9812b3ef6b4
Katherine Delgado <83666180+Kate505@users.noreply.github.com>
2024-03-05 09:14:18 -0600

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
+import robotsTxt from "astro-robots-txt";
+import UnoCSS from "@unocss/astro";
+import icon from "astro-icon";
+
+import solidJs from "@astrojs/solid-js";
+import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://gianmarco.xyz/",
+  integrations: [
+    sitemap(),
+    robotsTxt({
+      sitemap: [
+        "https://gianmarco.xyz/sitemap-index.xml",
+        "https://gianmarco.xyz/sitemap-0.xml",
+      ],
+    }),
+    solidJs(),
+    UnoCSS({ injectReset: true }),
+    icon()
+  ],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
+  output: "server",
+  adapter: netlify(),
+});
